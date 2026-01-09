4b21f523c9c7396e491c1d9dd8e1f73de10c080b
rajat-das247365 <rd540032@gmail.com>
2026-01-09 09:34:59 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     //responsive 3-column layout using css grid
 
-   /* <div className='bg-slate-700 w-full min-h-screen text-slate-200 flex justify-center items-center'>
+   <div className='bg-slate-700 w-full min-h-screen text-slate-200 flex justify-center items-center'>
       <div className='w-150 max-w-4xl  grid grid-flow-row grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 gap-4'>
       <div className=' border h-40'></div>
       <div className=' border h-40'></div>
@@ -16,10 +16,10 @@ const App = () => {
       <div className=' border h-40'></div>
       <div className=' border h-40'></div>
       </div>
-    </div> */
-    <div>
+      </div> 
+  
+     
 
-    </div>
   )
 }
 
